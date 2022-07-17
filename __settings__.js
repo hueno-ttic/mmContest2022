ab08d31d47c37d155e4395fbc0882719c237f18e
hueno-ttic <tsukasa.hiroshi@gmail.com>
2022-07-18 03:58:04 +0900

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1473313.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 90725933, 90725932, 90725901, 90725911, 90725918, 90725910, 90725888, 90725889, 90725934, 90725946, 90726070, 90726347, 90726635, 91234999, 91235000, 91235001, 91235002, 91235670, 91399870, 91400840, 91625940, 91625941, 91628413, 91826558, 92264000, 92396205, 92396798, 92396820, 92507046, 92528259, 92570060, 92648215, 93547281, 93547283, 93566694, 93809966, 93810086, 93810197, 93810624, 93829019, 93836912, 93837186, 93837188, 93846827 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/90725890/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/90725898/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/90725883/1/ammo.js', 'preload' : true},
+];
